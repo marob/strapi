eae4c3efd63a1f8489ea44e2e4ccd2546f383579
Simone Taeggi <startae14@gmail.com>
2023-01-27 10:57:23 +0100

    refactor the code to remove eslint comments

--- packages/core/admin/admin/src/content-manager/hooks/useFetchContentTypeLayout/index.js
+++ packages/core/admin/admin/src/content-manager/hooks/useFetchContentTypeLayout/index.js
@@ -1,7 +1,7 @@
 import { useCallback, useEffect, useMemo, useReducer, useRef } from 'react';
 import { useSelector, shallowEqual } from 'react-redux';
 import axios from 'axios';
-import { useFetchClient } from '@strapi/helper-plugin';
+import { getFetchClient } from '@strapi/helper-plugin';
 import formatLayouts from './utils/formatLayouts';
 import reducer, { initialState } from './reducer';
 import { makeSelectModelAndComponentSchemas } from '../../pages/App/selectors';
@@ -12,10 +12,12 @@ const useFetchContentTypeLayout = (contentTypeUID) => {
   const schemasSelector = useMemo(makeSelectModelAndComponentSchemas, []);
   const { schemas } = useSelector((state) => schemasSelector(state), shallowEqual);
   const isMounted = useRef(true);
-  const { get } = useFetchClient();
 
   const getData = useCallback(
     async (uid, source) => {
+      // TODO: evaluate to replace it with a useFetchClient when we work on the useCallback to remove
+      const { get } = getFetchClient();
+
       if (layouts[uid]) {
         dispatch({ type: 'SET_LAYOUT_FROM_STATE', uid });
 
@@ -48,8 +50,6 @@ const useFetchContentTypeLayout = (contentTypeUID) => {
         }
       }
     },
-    // TODO: remove when we evaluate the need of the useCallback
-    // eslint-disable-next-line react-hooks/exhaustive-deps
     [layouts, schemas]
   );
 
