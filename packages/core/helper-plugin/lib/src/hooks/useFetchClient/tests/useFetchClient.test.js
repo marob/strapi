c395813acdecf0794e7c5b98d5b66dfed1fad9b1
Simone Taeggi <startae14@gmail.com>
2023-01-31 14:25:19 +0100

    add test

--- packages/core/helper-plugin/lib/src/hooks/useFetchClient/tests/useFetchClient.test.js
+++ packages/core/helper-plugin/lib/src/hooks/useFetchClient/tests/useFetchClient.test.js
@@ -1,21 +1,59 @@
-import { renderHook } from '@testing-library/react-hooks';
+import React, { useEffect } from 'react';
+import { renderHook, act } from '@testing-library/react-hooks';
+import { useFetchClient } from '@strapi/helper-plugin';
 
-import useFetchClient from '../index';
+jest.mock('@strapi/helper-plugin', () => ({
+  ...jest.requireActual('@strapi/helper-plugin'),
+  useFetchClient: jest.fn().mockReturnValue({
+    get: jest.fn().mockResolvedValue({
+      data: {
+        results: [
+          { id: 2, name: 'newest', publishedAt: null },
+          { id: 1, name: 'oldest', publishedAt: null },
+        ],
+        pagination: { page: 1, pageCount: 10 },
+      },
+    }),
+    post: jest.fn(),
+    put: jest.fn(),
+    del: jest.fn(),
+  }),
+}));
 
-function setup() {
-  return renderHook(() => useFetchClient());
+const TestComponent = (props) => {
+  const { get } = useFetchClient();
+  useEffect(() => {
+    get('/foo');
+  }, [get]);
+
+  // eslint-disable-next-line react/prop-types
+  return <div {...props}>{props.children}</div>;
+};
+
+function setup(props) {
+  return new Promise((resolve) => {
+    act(() => {
+      resolve(
+        renderHook(() => useFetchClient(), {
+          wrapper: ({ children }) => <TestComponent {...props}>{children}</TestComponent>,
+        })
+      );
+    });
+  });
 }
 
 describe('useFetchClient', () => {
   afterEach(() => {
     jest.clearAllMocks();
   });
-  it('Should contain the get, del, put, post methods', async () => {
+  it('Should contain the get, del, put, post methods and call once the GET method', async () => {
     const { result } = await setup();
 
     expect(result.current).toHaveProperty('get');
     expect(result.current).toHaveProperty('post');
     expect(result.current).toHaveProperty('put');
     expect(result.current).toHaveProperty('del');
+
+    expect(result.current.get).toHaveBeenCalledTimes(1);
   });
 });
